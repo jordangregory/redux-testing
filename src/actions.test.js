288afc9f44691292f905c55a7b79dc96de832357
Jordan Gregory <jordangregory@gmail.com>
2017-08-08 11:49:29 -0500

    inital commit

---
+++ src/actions.test.js
@@ -0,0 +1,26 @@
+import { createTodo } from "./actions";
+import { toggleTodo } from "./actions";
+import { filterTodos } from "./actions";
+
+describe("CREATE_TODO yields a type text", () => {
+  it("should call a CREATE_TODO action", () => {
+    expect(createTodo("test")).toEqual({
+      type: "CREATE_TODO",
+      payload: "test"
+    });
+  });
+
+  it("should call a TOGGLE_TODO action", () => {
+    expect(toggleTodo("test")).toEqual({
+      type: "TOGGLE_TODO",
+      payload: "test"
+    });
+  });
+
+  it("should call a FILTER_TODOS action", () => {
+    expect(filterTodos("test")).toEqual({
+      type: "FILTER_TODOS",
+      payload: "test"
+    });
+  });
+});
